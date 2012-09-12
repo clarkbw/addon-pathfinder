5cc7eb17ba3b983accb4c6acdaa314b6dba3f19c
Erik Vold <erikvvold@gmail.com>
2012-09-11 22:05:24 -0700

    updating unload+ and tests

--- tests/test-unload+.js
+++ tests/test-unload+.js
@@ -1,39 +1,12 @@
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Initial Developer of the Original Code is Erik Vold
- * Portions created by the Initial Developer are Copyright (C) 2011
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *   Erik Vold <erikvvold@gmail.com> (Original Author)
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+"use strict";
 
 var timer = require("timer");
 var {Cc,Ci} = require("chrome");
+const windowUtils = require("window-utils");
+const { Loader } = require('test-harness/loader');
 
 function makeEmptyWindow() {
   var xulNs = "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul";
@@ -51,7 +24,7 @@ function makeEmptyWindow() {
 }
 
 exports.testUnloading = function(test) {
-  var loader = test.makeSandboxedLoader();
+  var loader = Loader(module);
   var {unload} = loader.require("unload+");
   var unloadCalled = 0;
 
@@ -76,9 +49,10 @@ exports.testUnloading = function(test) {
 };
 
 exports.testUnloadingWindow = function(test) {
-  var loader = test.makeSandboxedLoader();
+  test.waitUntilDone();
+
+  var loader = Loader(module);
   var {unload} = loader.require("unload+");
-  var windowUtils = loader.require("window-utils");
   var unloadCalled = 0;
   var finished = false;
   var myWindow;
@@ -88,7 +62,7 @@ exports.testUnloadingWindow = function(test) {
       if (window == myWindow) {
         test.pass("onTrack() called with our test window");
 
-        function unloader() {
+        let unloader = function unloader() {
           unloadCalled++;
         }
         unload(unloader, window);
@@ -142,5 +116,4 @@ exports.testUnloadingWindow = function(test) {
 
   var wt = new windowUtils.WindowTracker(delegate);
   myWindow = makeEmptyWindow();
-  test.waitUntilDone(5000);
-}
+};
