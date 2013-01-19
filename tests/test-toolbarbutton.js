27ae023178bd91d2825647fd0f41e93cb16cea21
Erik Vold <erikvvold@gmail.com>
2013-01-19 15:55:27 -0800

    adding tests for #18

--- tests/test-toolbarbutton.js
+++ tests/test-toolbarbutton.js
@@ -3,21 +3,27 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 'use strict';
 
-const windowUtils = require("window-utils");
+const windows = require("windows").browserWindows;
 const toolbarbutton = require("toolbarbutton");
+//const { Loader } = require('sdk/test/loader');
+const winUtils = require('sdk/window/utils');
 
 const NS_XUL = "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul";
 
 const TEST_ICON_URL = module.uri.replace(/[^\.\\\/]*\.js$/, "test.png");
 const TEST_ICON_BLACK_URL = module.uri.replace(/[^\.\\\/]*\.js$/, "black.png");
 
-function $(id) windowUtils.activeBrowserWindow.document.getElementById(id);
+function $(id) winUtils.getMostRecentBrowserWindow().document.getElementById(id);
 
 function createToolbarButton(options, test) {
   test.assertEqual(!$(options.id), true);
+
   var tbb = toolbarbutton.ToolbarButton(options);
   test.assertEqual(!$(options.id), true);
+
   tbb.moveTo(options);
+  test.assertEqual(!$(options.id), false);
+
   return tbb;
 }
 
@@ -108,4 +114,51 @@ exports.testTBBPropertyChange = function(test) {
   test.done();
 };
 
+exports.testTBBIteratorWithNonBrowserWindow = function(test) {
+  test.waitUntilDone();
 
+  let scratchpad = winUtils.getMostRecentBrowserWindow().Scratchpad.openScratchpad();
+  let options = {
+    id: "test-tbb5",
+    label: "TEST",
+    toolbarID: "nav-bar",
+    image: TEST_ICON_URL
+  };
+  windows.open({
+    onOpen: function(window) {
+      let tbb = createToolbarButton(options, test);
+      test.assertEqual(windows.length, 2);
+
+      scratchpad.close();
+      tbb.destroy();
+      window.close(function() test.done());
+    }
+  })
+};
+
+exports.testTBBIterator2 = function(test) {
+  test.waitUntilDone();
+
+  let window1 = winUtils.getMostRecentBrowserWindow();
+  let options = {
+    id: "test-tbb6",
+    label: "TEST",
+    toolbarID: "nav-bar",
+    image: TEST_ICON_URL
+  };
+  let button = window1.document.createElementNS(NS_XUL, "toolbarbutton");
+  button.setAttribute('id', options.id);
+  window1.document.getElementById(options.toolbarID).appendChild(button);
+  test.assert(!!$(options.id));
+
+  windows.open({
+    onOpen: function(window) {
+      let tbb = createToolbarButton(options, test);
+      test.assertEqual(windows.length, 2);
+
+      tbb.destroy();
+      button.parentNode.removeChild(button);
+      window.close(function() test.done());
+    }
+  })
+};
