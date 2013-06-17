08f29afb0bdbca23fd10f2d244c09d2c85ec6b74
Erik Vold <erikvvold@gmail.com>
2013-06-16 18:03:09 -0700

    adding about:what unload test for pathfinder/scheme/about

--- test/test-scheme-about.js
+++ test/test-scheme-about.js
@@ -3,6 +3,23 @@
 const { Loader } = require('sdk/test/loader');
 const tabs = require('sdk/tabs')
 
+function openTabGetContent(url, callback) {
+  tabs.open({
+    url: 'about:test',
+    inBackground: true,
+    onReady: function(tab) {
+      let worker = tab.attach({
+        contentScript: 'self.port.emit("body", document.body.innerHTML)'
+      })
+      worker.port.on('body', function(msg) {
+        tab.close(function() {
+          callback(msg);
+        });
+      });
+    }
+  })
+}
+
 exports.testAddAboutWhat = function(assert, done) {
   const loader = Loader(module);
   const { add } = loader.require('pathfinder/scheme/about');
@@ -12,22 +29,15 @@ exports.testAddAboutWhat = function(assert, done) {
     url: 'data:text/html;charset=utf-8,<body>test</body>'
   });
 
-  tabs.open({
-  	url: 'about:test',
-  	inBackground: true,
-  	onReady: function(tab) {
-      let worker = tab.attach({
-        contentScript: 'self.port.emit("body", document.body.innerHTML)'
-      })
-      worker.port.on('body', function(msg) {
-        assert.equal(msg, 'test', 'about:test content is "test"');
-        tab.close(function() {
-          loader.unload();
-          done();
-        });
-      });
-  	}
-  })
+  openTabGetContent('about:test', function(msg) {
+    assert.equal(msg, 'test', 'about:test content is "test"');
+    loader.unload();
+    openTabGetContent('about:test', function(msg) {
+      assert.notEqual(msg, 'test', 'about:test content is "test"');
+      done();
+    });
+  });
+
 }
 
 require('test').run(exports);
