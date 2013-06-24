3cfd7acabe4a3333911de8165d15c00e05645ad3
Erik Vold <erikvvold@gmail.com>
2013-06-24 11:10:31 -0700

    converting userstyles tests to the new test framework

--- test/test-userstyles.js
+++ test/test-userstyles.js
@@ -3,64 +3,67 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 'use strict';
 
-const userstyles = require('userstyles');
-const { Loader } = require("sdk/test/loader");
+const { Loader } = require('sdk/test/loader');
 
-const TEST_CSS_URL = module.uri.replace(/\.js$/, ".css");
-const TEST_FNF_URL = module.uri.replace(/\.js$/, ".x.css");
+const userstyles = require('userstyles');
+
+const TEST_CSS_URL = module.uri.replace(/\.js$/, '.css');
+const TEST_FNF_URL = module.uri.replace(/\.js$/, '.x.css');
 
 // TEST: userstyles.load
-exports.testLoad = function(test) {
-  test.assertEqual(userstyles.registered(TEST_CSS_URL), false, 'css is unregistered.');
+exports.testLoad = function(assert) {
+  assert.equal(userstyles.registered(TEST_CSS_URL), false, 'css is unregistered.');
 
   userstyles.load(TEST_CSS_URL);
-  test.assert(userstyles.registered(TEST_CSS_URL), 'css was registered.');
+  assert.ok(userstyles.registered(TEST_CSS_URL), 'css was registered.');
 
   userstyles.unload(TEST_CSS_URL);
-  test.assertEqual(userstyles.registered(TEST_CSS_URL), false, 'css was unregistered.');
+  assert.equal(userstyles.registered(TEST_CSS_URL), false, 'css was unregistered.');
 };
 
 // TEST: userstyles.load file not found
-exports.testLoadFNF = function(test) {
-  test.assertEqual(userstyles.registered(TEST_CSS_URL), false, 'css is not registered.');
+exports.testLoadFNF = function(assert) {
+  assert.equal(userstyles.registered(TEST_CSS_URL), false, 'css is not registered.');
 
   try {
     userstyles.load(TEST_FNF_URL);
-    test.fail('trying to load a file that does not exist should throw an error');
+    assert.fail('trying to load a file that does not exist should throw an error');
  }
  catch(e) {
-   test.pass('trying to load a file that does not exist throws an error');
+   assert.pass('trying to load a file that does not exist throws an error');
  }
 
-  test.assertEqual(userstyles.registered(TEST_CSS_URL), false, 'css was not registered.');
+  assert.equal(userstyles.registered(TEST_CSS_URL), false, 'css was not registered.');
 };
 
 // TEST: userstyles.load for 'agent' type
-exports.testLoadAgent = function(test) {
-  test.assertEqual(userstyles.registered(TEST_CSS_URL), false, 'css is not registered.');
-  test.assertEqual(userstyles.registered(TEST_CSS_URL, {type: 'agent'}), false, 'css is not registered.');
+exports.testLoadAgent = function(assert) {
+  assert.equal(userstyles.registered(TEST_CSS_URL), false, 'css is not registered.');
+  assert.equal(userstyles.registered(TEST_CSS_URL, {type: 'agent'}), false, 'css is not registered.');
 
   userstyles.load(TEST_CSS_URL, {type: 'AgeNt'});
-  test.assert(userstyles.registered(TEST_CSS_URL, {type: 'AGENT'}), 'css was registered.');
+  assert.ok(userstyles.registered(TEST_CSS_URL, {type: 'AGENT'}), 'css was registered.');
 
   try {
     userstyles.unload(TEST_CSS_URL);
-    test.fail('unregister did not throw an error');
+    assert.fail('unregister did not throw an error');
   }
   catch(e) {
-    test.pass('unregister did throw an error');
+    assert.pass('unregister did throw an error');
   }
-  test.assertEqual(userstyles.registered(TEST_CSS_URL, {type: 'agent'}), true, 'css was not unregistered.');
+  assert.equal(userstyles.registered(TEST_CSS_URL, {type: 'agent'}), true, 'css was not unregistered.');
 
   userstyles.unload(TEST_CSS_URL, {type: 'agent'});
-  test.assertEqual(userstyles.registered(TEST_CSS_URL, {type: 'agent'}), false, 'css was unregistered.');
+  assert.equal(userstyles.registered(TEST_CSS_URL, {type: 'agent'}), false, 'css was unregistered.');
 };
 
-exports.testUnload = function(test) {
-  test.assertEqual(userstyles.registered(TEST_CSS_URL), false, 'css is unregistered.');
+exports.testUnload = function(assert) {
+  assert.equal(userstyles.registered(TEST_CSS_URL), false, 'css is unregistered.');
   let loader = Loader(module);
   loader.require('userstyles').load(TEST_CSS_URL);
-  test.assert(userstyles.registered(TEST_CSS_URL), 'css was registered.');
+  assert.ok(userstyles.registered(TEST_CSS_URL), 'css was registered.');
   loader.unload();
-  test.assertEqual(userstyles.registered(TEST_CSS_URL), false, 'css was unregistered.');
+  assert.equal(userstyles.registered(TEST_CSS_URL), false, 'css was unregistered.');
 }
+
+require('sdk/test').run(exports);
